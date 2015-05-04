8df9a087b870c39b638ac5b0bbaed8f90ef0e6f1
boxofsquares <janik_schambes@yahoo.de>
2015-05-04 13:38:17 -0700

    Second try.

---
+++ SerialCommunication/Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+    Public fileStream As StreamWriter = New IO.StreamWriter("serialFile.txt", True)
+    Public Sub Main()
+        Dim myPort As SerialPort = New SerialPort("COM1", 4800)
+
+
+        AddHandler myPort.DataReceived, AddressOf DataReceivedHandler
+
+
+        myPort.Open()
+        Console.WriteLine("Port opened.")
+        Console.ReadLine()
+        Console.WriteLine("User stop.")
+        Console.ReadLine()
+
+        fileStream.Write(vbNewLine)
+        fileStream.Close()
+
+    End Sub
+
+    Private Sub DataReceivedHandler(sender As Object, e As SerialDataReceivedEventArgs)
+        Dim port1 As SerialPort = CType(sender, SerialPort)
+        Dim indata As String = port1.ReadExisting
+        If Asc(indata) = 13 Then
+            Console.Write(vbNewLine)
+            fileStream.Write(vbNewLine)
+        End If
+        Console.Write(indata)
+        fileStream.Write(indata)
+
+    End Sub
+End Module
